a9665e9f208d5da451910b2be914d43ed59ee17d
gjkim <gjkim@softhills.net>
2025-05-04 15:14:44 +0900

    1st Example

---
+++ Manual/MyControl/MyClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PluginConnector;
+
+namespace MyControl
+{
+    public class MyClass : PluginConnector.IEntryConnector
+    {
+        private PluginConnector.IPluginService pluginHost;
+
+        public MyClass()
+        {
+        }
+
+        public MyClass(IPluginService host) : this()
+        {
+            pluginHost = host;
+
+            pluginHost.OnInitializedAppEvent += PluginHost_OnInitializedAppEvent;
+            pluginHost.OnModelOpenedEvent += PluginHost_OnModelOpenedEvent;
+        }
+
+        private void PluginHost_OnInitializedAppEvent(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Plugin-OnInitializedAppEvent");
+        }
+
+        private void PluginHost_OnModelOpenedEvent(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Plugin-OnModelOpenedEvent");
+
+            VIZCore3DX.Core vizcore3dx = (VIZCore3DX.Core)pluginHost.GetVIZCore3DX();
+        }
+    }
+}
---
+++ Manual/MyPlugin/PluginUserControl.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using PluginConnector;
+
+namespace MyPlugin
+{
+    public partial class PluginUserControl : UserControl, PluginConnector.IEntryConnector
+    {
+        private PluginConnector.IPluginService pluginHost;
+        private VIZCore3DX.Core vizcore3dx;
+
+        public PluginUserControl()
+        {
+            InitializeComponent();
+        }
+
+        public PluginUserControl(IPluginService host): this()
+        {
+            pluginHost = host;
+
+            pluginHost.OnInitializedAppEvent += PluginHost_OnInitializedAppEvent;
+            pluginHost.OnModelOpenedEvent += PluginHost_OnModelOpenedEvent;
+        }
+
+        private void PluginHost_OnInitializedAppEvent(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void PluginHost_OnModelOpenedEvent(object sender, EventArgs e)
+        {
+            vizcore3dx = (VIZCore3DX.Core)pluginHost.GetVIZCore3DX();
+        }
+    }
+}
